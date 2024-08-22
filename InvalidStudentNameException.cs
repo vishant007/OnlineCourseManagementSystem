65bcdddff708c4229d5d3e55f3124acff6003dcc
Sunil6201 <sunilreddysunilreddy12345@gmail.com>
2024-08-22 11:26:39 +0530

    Course Not Found Exception

--- InvalidStudentNameException.cs
+++ InvalidStudentNameException.cs
@@ -1,7 +1,14 @@
 using System;
 
+/// <summary>
+/// Represents errors that occur when an invalid student name is provided.
+/// </summary>
 public class InvalidStudentNameException : Exception
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="InvalidStudentNameException"/> class with a specified error message.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
     public InvalidStudentNameException(string message) : base(message)
     {
     }
--- Student.cs
+++ Student.cs
@@ -1,25 +1,37 @@
 using System.Diagnostics.Metrics;
-
-public class Student {
-    public int Id{get;set;}
-    public string Name{get;set;}
-    public string Email{get;set;}
-
-    public Student(){
-        
-    }
-
-    // public Student(int id,string name,string email){
-    //     this.Id=id;
-    //     this.Name=name;
-    //     this.Email=Email;
-    // }
-    public override string ToString()
-
+/// <summary>
+/// Represents a student with an ID, name, and email address.
+/// </summary>
+public class Student
     {
-        return $"Id:{Id} Name:{Name} Email:{Email}";
-    }
-    
-    
+    /// <summary>
+    /// Gets or sets the unique identifier for the student.
+     /// </summary>
+    public int Id { get; set; }
 
+    /// <summary>
+    /// Gets or sets the name of the student.
+    /// </summary>
+     public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the email address of the student.
+    /// </summary>
+    public string Email { get; set; }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Student"/> class.
+    /// </summary>
+    public Student()
+    {
+        // Default constructor
+    }
+    /// <summary>
+    /// Returns a string that represents the current <see cref="Student"/> object.
+    /// </summary>
+    /// <returns>A string that represents the current <see cref="Student"/> object.</returns>
+    public override string ToString()
+    {
+         return $"Id: {Id} Name: {Name} Email: {Email}";
+    }
 }
